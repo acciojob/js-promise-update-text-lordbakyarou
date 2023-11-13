c315656ec41821deef577025ec0c2cbdb7f45add
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-11-14 03:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const outpu = document.getElementById("output");
+
+function pro() {
+  return new Promise((res, rej) => {
+    if (true) {
+      setTimeOut(() => {
+        res("Hello, world!");
+      }, 1000);
+    }
+  });
+}
+outpu.innerText = pro()
+  .then((data) => {
+    return data;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
