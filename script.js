ef13d2dbad3be39b5b31dda4ccc17c72a62a2de2
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-11-14 03:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,15 +14,10 @@ let hey;
 
 const her = pro()
   .then((data) => {
-    return data;
-  }).then((data)=>{
-		hey = data;
-	  outpu.innerText = hey
-	  return data;
+	  outpu.innerText = data;
   })
   .catch((err) => {
     console.log(err);
   });
 
-console.log(hey);
 
